aaa30e402b1400f652cd68362e633f5282cb83be
Steffan Andrews <stef@orchetect.com>
2021-01-17 01:12:23 -0800

    Bumped dependency ver

--- Package.swift
+++ Package.swift
@@ -8,20 +8,16 @@ let package = Package(
     name: "OTCore",
 	
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OTCore",
             targets: ["OTCore"]),
     ],
 	
     dependencies: [
-		// Dependencies declare other packages that this package depends on.
-		.package(url: "https://github.com/orchetect/SegmentedProgress", from: "1.0.0"),
+		.package(url: "https://github.com/orchetect/SegmentedProgress", from: "1.0.1"),
 	],
 	
     targets: [
-		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
-		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
 			name: "OTCore",
 			dependencies: []),
