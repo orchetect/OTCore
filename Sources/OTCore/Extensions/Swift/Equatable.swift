694986a02c1933e0ef8bceb0f60d0265a6cb587e
Steffan Andrews <stef@orchetect.com>
2021-01-20 22:36:36 -0800

    Added .transform { } and .if(:then:) and .if(:then:else:)

---
+++ Sources/OTCore/Extensions/Swift/Equatable.swift
@@ -0,0 +1,212 @@
+//
+//  Equatable.swift
+//  OTCore
+//
+//  Created by Steffan Andrews on 2021-01-20.
+//  Copyright © 2021 Steffan Andrews. All rights reserved.
+//
+
+// MARK: - .transform { }
+
+extension Equatable {
+	
+	/// **OTCore:**
+	/// Transforms a value using a closure.
+	/// (Functional convenience method)
+	///
+	/// Example 1 - simple operation:
+	///
+	///     123.transform { $0 * 2 } // 246
+	///
+	/// Example 2 - different resulting type:
+	///
+	///     123.transform { "\($0)" } // "123"
+	///
+	/// Example 3 - complex transform block
+	///
+	///     123.transform { (source) -> Bool in
+	///         let calc = Double(source) * 2 / 14.9
+	///         let rounded = round(calc)
+	///         return rounded > 18
+	///     }
+	///
+	/// Example 4 - chainable:
+	///
+	///     123.transform { $0 * 2 / 12.47 }
+	///         .rounded()
+	///         .transform { "Rounded value: \($0)" }
+	///     // "Rounded value: 20.0"
+	@inlinable public
+	func transform<T>(_ transform: (Self) throws -> T) rethrows -> T {
+		
+		try transform(self)
+		
+	}
+	
+}
+
+
+// MARK: - .if(then:)
+
+extension Equatable {
+	
+	/// **OTCore:**
+	/// If-statement producing a new value if the `test` closure evaluates as `true`, otherwise returns `nil`.
+	/// (Functional convenience method)
+	///
+	/// Example:
+	///
+	///     123.if({ $0 > 100 }, then: "yes") // Optional("yes")
+	///
+	///     123.if({ $0 < 100 }, then: "yes") // nil
+	///
+	@inlinable public func `if`<T>(_ test: (Self) throws -> Bool,
+								   then trueValue: T) rethrows -> T? {
+		
+		try test(self) ? trueValue : nil
+		
+	}
+	
+	/// **OTCore:**
+	/// If-statement producing a transformed value if the `test` closure evaluates as `true`, otherwise returns `nil`.
+	/// `self` is passed to the `trueValue` closure so the source value can be propagated.
+	/// (Functional convenience method)
+	///
+	/// Example:
+	///
+	///     123.if({ $0 > 100 }, then: { "\($0 * 2)" }) // Optional("246")
+	///
+	///     123.if({ $0 < 100 }, then: { "\($0 * 2)" }) // nil
+	///
+	@inlinable public func `if`<T>(_ test: (Self) throws -> Bool,
+								   then trueValue: (Self) throws -> T) rethrows -> T? {
+		
+		try test(self) ? trueValue(self) : nil
+		
+	}
+	
+}
+
+
+// MARK: - .if(then:else:)
+
+extension Equatable {
+	
+	/// **OTCore:**
+	/// If-statement producing one of two values depending on the `test` closure.
+	/// (Functional convenience method)
+	///
+	/// Example 1 - single line:
+	///
+	///     123.if({ $0 > 100 }, then: "yes", else: "no") // "yes"
+	///
+	///     123.if({ $0 < 100 }, then: "yes", else: "no") // "no"
+	///
+	/// Example 2 - one clause per line:
+	///
+	///     123.if({ $0 > 100 },
+	///            then: { "yes" },
+	///            else: { "no" })
+	///     // "no"
+	///
+	/// Example 3 - using trailing closure syntax:
+	///
+	///     let number = 50
+	///
+	///     number
+	///         .if {
+	///             $0 > 100
+	///	        } then: {
+	///             "yes"
+	///	        } else: {
+	///             "no"
+	///	        }
+	///	    // "no"
+	///
+	@inlinable public func `if`<T>(_ test: (Self) throws -> Bool,
+								   then trueValue: T,
+								   else falseValue: T) rethrows -> T {
+		
+		try test(self) ? trueValue : falseValue
+		
+	}
+	
+	/// **OTCore:**
+	/// If-statement producing one of two values depending on the `test` closure.
+	/// `self` is passed to the resulting closures so the source value can be propagated.
+	/// (Functional convenience method)
+	///
+	/// Example 1 - single line:
+	///
+	///     123.if({ $0 > 100 }, then: { "high: \($0)" }, else: { "low: \($0)" })
+	///     // "high: 123"
+	///
+	/// Example 2 - one clause per line:
+	///
+	///     123.if({ $0 > 100 },
+	///            then: { "high: \($0)" },
+	///            else: { "low: \($0)" })
+	///     // "high: 123"
+	///
+	/// Example 3 - using trailing closure syntax:
+	///
+	///     let number = 50
+	///
+	///     number
+	///         .if {
+	///             $0 > 100
+	///	        } then: {
+	///             "high: \($0)"
+	///	        } else: {
+	///             "low: \($0)"
+	///	        }
+	///	    // "low: 50"
+	///
+	@inlinable public func `if`<T>(_ test: (Self) throws -> Bool,
+								   then trueValue: (Self) throws -> T,
+								   else falseValue: (Self) throws -> T) rethrows -> T {
+		
+		try test(self) ? trueValue(self) : falseValue(self)
+		
+	}
+	
+	/// **OTCore:**
+	/// If-statement producing one of two values depending on the `test` closure.
+	/// `self` is passed to the resulting closures so the source value can be propagated.
+	/// (Functional convenience method)
+	///
+	/// Example 1 - single line:
+	///
+	///     123.if({ $0 > 100 }, then: { "high: \($0)" }, else: { "low: \($0)" })
+	///     // "high: 123"
+	///
+	/// Example 2 - one clause per line:
+	///
+	///     123.if({ $0 > 100 },
+	///            then: { "high: \($0)" },
+	///            else: { "low: \($0)" })
+	///     // "high: 123"
+	///
+	/// Example 3 - using trailing closure syntax:
+	///
+	///     let number = 50
+	///
+	///     number
+	///         .if {
+	///             $0 > 100
+	///	        } then: {
+	///             "high: \($0)"
+	///	        } else: {
+	///             "low: \($0)"
+	///	        }
+	///	    // "low: 50"
+	///
+	@inlinable public func `if`(_ test: (Self) throws -> Bool,
+								then trueValue: (Self) throws -> Self,
+								else falseValue: (Self) throws -> Self) rethrows -> Self {
+		
+		try test(self) ? trueValue(self) : falseValue(self)
+		
+	}
+	
+}
---
+++ Tests/OTCoreTests/Extensions/Swift/Equatable Tests.swift
@@ -0,0 +1,157 @@
+//
+//  Equatable Tests.swift
+//  OTCore
+//
+//  Created by Steffan Andrews on 2021-01-20.
+//  Copyright © 2021 Steffan Andrews. All rights reserved.
+//
+
+#if !os(watchOS)
+
+import XCTest
+@testable import OTCore
+
+class Extensions_Swift_Equatable_Tests: XCTestCase {
+	
+	override func setUp() { super.setUp() }
+	override func tearDown() { super.tearDown() }
+	
+	func testTransform() {
+		
+		// emitting same type as self
+		
+		XCTAssertEqual (
+			123.transform { $0 * 2 },
+			246
+		)
+		
+		// emitting different type than self
+		
+		XCTAssertEqual (
+			123.transform { "\($0 * 2)" },
+			"246"
+		)
+		
+	}
+	
+	func testIf_ThenValue() {
+		
+		// emitting same type as self
+		
+		XCTAssertEqual (
+			123.if({ $0 > 100 }, then: 50),
+			50
+		)
+		
+		XCTAssertEqual (
+			123.if({ $0 < 100 }, then: 50),
+			nil
+		)
+		
+		// emitting different type than self
+		
+		XCTAssertEqual (
+			123.if({ $0 > 100 }, then: "yes"),
+			"yes"
+		)
+		
+		XCTAssertEqual (
+			123.if({ $0 < 100 }, then: "yes"),
+			nil
+		)
+		
+	}
+	
+	func testIf_ThenClosure() {
+		
+		// emitting same type as self
+		
+		XCTAssertEqual (
+			123.if({ $0 > 100 }, then: { $0 * 2 }),
+			246
+		)
+		
+		XCTAssertEqual (
+			123.if({ $0 < 100 }, then: { $0 * 2 }),
+			nil
+		)
+		
+		// emitting different type than self
+		
+		XCTAssertEqual (
+			123.if({ $0 > 100 }, then: { "\($0 * 2)" }),
+			"246"
+		)
+		
+		XCTAssertEqual (
+			123.if({ $0 < 100 }, then: { "\($0 * 2)" }),
+			nil
+		)
+		
+	}
+	
+	func testIf_ThenValue_ElseValue() {
+		
+		// emitting same type as self
+		
+		XCTAssertEqual (
+			123.if({ $0 > 100 }, then: 50, else: 200),
+			50
+		)
+		
+		XCTAssertEqual (
+			123.if({ $0 < 100 }, then: 50, else: 200),
+			200
+		)
+		
+		// emitting different type than self
+		
+		XCTAssertEqual (
+			123.if({ $0 > 100 }, then: "yes", else: "no"),
+			"yes"
+		)
+		
+		XCTAssertEqual (
+			123.if({ $0 < 100 }, then: "yes", else: "no"),
+			"no"
+		)
+		
+	}
+	
+	func testIf_ThenClosure_ElseClosure() {
+		
+		// emitting same type as self
+		
+		XCTAssertEqual (
+			123.if({ $0 > 100 },
+				   then: { $0 + 2 },
+				   else: { $0 - 2 }),
+			125)
+		
+		XCTAssertEqual (
+			123.if({ $0 < 100 },
+				   then: { $0 + 2 },
+				   else: { $0 - 2 }),
+			121)
+		
+		// emitting different type than self
+		
+		XCTAssertEqual (
+			123.if({ $0 > 100 },
+				   then: { "yes \($0)" },
+				   else: { "no \($0)" }),
+			"yes 123"
+		)
+		
+		XCTAssertEqual (
+			123.if({ $0 < 100 },
+				   then: { "yes \($0)" },
+				   else: { "no \($0)" }),
+			"no 123"
+		)
+		
+	}
+	
+}
+
+#endif
