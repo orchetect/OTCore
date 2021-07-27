28c33a076b0d0f94fd3fd50d203097b59263cee4
Steffan Andrews <stef@orchetect.com>
2021-07-27 00:09:36 -0700

    Rewrote `XCTWait` with alternate mechanism

--- Sources/OTCore/XCTest/XCTWait.swift
+++ Sources/OTCore/XCTest/XCTWait.swift
@@ -15,21 +15,25 @@ extension XCTestCase {
     @available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
     public func XCTWait(sec timeout: Double) {
         
-        var inTime = timespec()
-        _ = clock_gettime(CLOCK_MONOTONIC_RAW, &inTime)
+        RunLoop.current.run(until: .init(timeIntervalSinceNow: timeout))
         
-        let delayExpectation = XCTestExpectation()
-        delayExpectation.isInverted = true
+        // old method:
         
-        var adjustTime = timespec()
-        _ = clock_gettime(CLOCK_MONOTONIC_RAW, &adjustTime)
-        
-        let newTimeout =
-            timeout
-            - (adjustTime.doubleValue - inTime.doubleValue)
-            - 0.0015
-        
-        wait(for: [delayExpectation], timeout: newTimeout)
+        // var inTime = timespec()
+        // _ = clock_gettime(CLOCK_MONOTONIC_RAW, &inTime)
+        //
+        // let delayExpectation = XCTestExpectation()
+        // delayExpectation.isInverted = true
+        //
+        // var adjustTime = timespec()
+        // _ = clock_gettime(CLOCK_MONOTONIC_RAW, &adjustTime)
+        //
+        // let newTimeout =
+        //     timeout
+        //     - (adjustTime.doubleValue - inTime.doubleValue)
+        //     - 0.00115
+        //
+        // wait(for: [delayExpectation], timeout: newTimeout)
         
     }
     
