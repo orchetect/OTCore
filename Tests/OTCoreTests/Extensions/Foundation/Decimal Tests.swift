67e89c9645b0d64b8a752fbc7a8d911bc25872d6
Steffan Andrews <stef@orchetect.com>
2021-07-30 17:48:10 -0700

    Updated unit tests

--- Tests/OTCoreTests/Extensions/Foundation/Decimal Tests.swift
+++ Tests/OTCoreTests/Extensions/Foundation/Decimal Tests.swift
@@ -72,22 +72,30 @@ class Extensions_Foundation_Decimal_Tests: XCTestCase {
         
         // .rounded()
         
-        XCTAssertEqual(Decimal(1.1236).rounded(decimalPlaces: -1), Decimal(1.0))
-        XCTAssertEqual(Decimal(1.1236).rounded(decimalPlaces: 0),  Decimal(1.0))
-        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: 2), Decimal(string: "1.12")!)
-        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: 3), Decimal(string: "1.124")!)
-        XCTAssertEqual(Decimal(1.1236).rounded(decimalPlaces: 4),  Decimal(1.1236))
-        XCTAssertEqual(Decimal(1.1236).rounded(decimalPlaces: 5),  Decimal(1.1236))
+        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: -1), Decimal(string: "1.0")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: 0),  Decimal(string: "1.0")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: 1),  Decimal(string: "1.1")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: 2),  Decimal(string: "1.12")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: 3),  Decimal(string: "1.124")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: 4),  Decimal(string: "1.1236")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.rounded(decimalPlaces: 5),  Decimal(string: "1.1236")!)
         
         XCTAssertEqual(Decimal(string: "0.123456789")!.rounded(decimalPlaces: 8), Decimal(string: "0.12345679")!)
         
-        var dec = Decimal(0.1264)
-        dec.round(decimalPlaces: 2)
-        XCTAssertEqual(dec, Decimal(0.13))
+        XCTAssertEqual(Decimal(string: "-0.1")!.rounded(decimalPlaces: 0), Decimal(string: "-0.0")!)
+        XCTAssertEqual(Decimal(string: "-0.1")!.rounded(decimalPlaces: 1), Decimal(string: "-0.1")!)
+        XCTAssertEqual(Decimal(string: "-1.7")!.rounded(decimalPlaces: 0), Decimal(string: "-2.0")!)
+        XCTAssertEqual(Decimal(string: "-1.7")!.rounded(decimalPlaces: 1), Decimal(string: "-1.7")!)
         
-        dec = Decimal(0.1264)
+        // .round()
+        
+        var dec = Decimal(string: "0.1264")!
+        dec.round(decimalPlaces: 2)
+        XCTAssertEqual(dec, Decimal(string: "0.13")!)
+        
+        dec = Decimal(string: "0.1264")!
         dec.round(decimalPlaces: 3)
-        XCTAssertEqual(dec, Decimal(0.126))
+        XCTAssertEqual(dec, Decimal(string: "0.126")!)
         
     }
     
@@ -95,15 +103,22 @@ class Extensions_Foundation_Decimal_Tests: XCTestCase {
         
         // .truncated()
         
-        XCTAssertEqual(Decimal(1.1236).truncated(decimalPlaces: -1), Decimal(1.0))
-        XCTAssertEqual(Decimal(1.1236).truncated(decimalPlaces: 0),  Decimal(1.0))
-        //XCTAssertEqual(Decimal(1.1236).truncated(decimalPlaces: 2),  Decimal(1.12)) // becomes 1.1200000000000002048 because of the Double literal
-        XCTAssertEqual(Decimal(string: "1.1236")!.truncated(decimalPlaces: 2), Decimal(string: "1.12")!)
-        XCTAssertEqual(Decimal(1.1236).truncated(decimalPlaces: 3),  Decimal(1.123))
-        XCTAssertEqual(Decimal(1.1236).truncated(decimalPlaces: 4),  Decimal(1.1236))
-        XCTAssertEqual(Decimal(1.1236).truncated(decimalPlaces: 5),  Decimal(1.1236))
+        XCTAssertEqual(Decimal(string: "1.1236")!.truncated(decimalPlaces: -1), Decimal(string: "1.0")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.truncated(decimalPlaces: 0),  Decimal(string: "1.0")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.truncated(decimalPlaces: 1),  Decimal(string: "1.1")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.truncated(decimalPlaces: 2),  Decimal(string: "1.12")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.truncated(decimalPlaces: 3),  Decimal(string: "1.123")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.truncated(decimalPlaces: 4),  Decimal(string: "1.1236")!)
+        XCTAssertEqual(Decimal(string: "1.1236")!.truncated(decimalPlaces: 5),  Decimal(string: "1.1236")!)
         
-        XCTAssertEqual(Decimal(0.123456789).truncated(decimalPlaces: 8), Decimal(0.12345678))
+        XCTAssertEqual(Decimal(string: "0.123456789")!.truncated(decimalPlaces: 8), Decimal(string: "0.12345678")!)
+        
+        XCTAssertEqual(Decimal(string: "-0.1")!.truncated(decimalPlaces: 0), Decimal(string: "0")!)
+        XCTAssertEqual(Decimal(string: "-0.1")!.truncated(decimalPlaces: 1), Decimal(string: "-0.1")!)
+        XCTAssertEqual(Decimal(string: "-1.7")!.truncated(decimalPlaces: 0), Decimal(string: "-1.0")!)
+        XCTAssertEqual(Decimal(string: "-1.7")!.truncated(decimalPlaces: 1), Decimal(string: "-1.7")!)
+        
+        // .truncate()
         
         var dec = Decimal(0.1264)
         dec.truncate(decimalPlaces: 2)
