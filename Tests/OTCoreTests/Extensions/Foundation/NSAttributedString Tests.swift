8b01d003fa7477ba00a144a8a493f1c8ef77a1bb
Steffan Andrews <stef@orchetect.com>
2021-10-14 16:53:15 -0700

    Added `NSAttributedString` unit tests

--- Tests/OTCoreTests/Extensions/Foundation/NSAttributedString Tests.swift
+++ Tests/OTCoreTests/Extensions/Foundation/NSAttributedString Tests.swift
@@ -5,6 +5,12 @@
 
 #if !os(watchOS)
 
+#if os(macOS)
+import AppKit
+#else
+import UIKit
+#endif
+
 import XCTest
 @testable import OTCore
 
@@ -13,9 +19,51 @@ class Extensions_Foundation_NSAttributedString_Tests: XCTestCase {
     override func setUp() { super.setUp() }
     override func tearDown() { super.tearDown() }
     
-    func testNSAttributedString() {
+    func testNSAttributedString_addingAttribute() {
         
-        #warning("add tests")
+        let rawString = "Test"
+        
+        // vanilla NSAttributedString
+        let attrStr1 = NSAttributedString(string: rawString)
+        XCTAssertEqual(attrStr1.string, rawString)
+        XCTAssertEqual(attrStr1.getAlignmentAttributes(), [])
+        
+        // with alignment attribute
+        let attrStr2 = attrStr1.addingAttribute(alignment: .center)
+        XCTAssertEqual(attrStr2.string, rawString)
+        XCTAssertEqual(attrStr2.getAlignmentAttributes(), [.center])
+        
+    }
+    
+    func testNSAttributedString_addAttribute() {
+        
+        let rawString = "Test"
+        
+        // vanilla NSMutableAttributedString
+        let attrStr = NSMutableAttributedString(string: rawString)
+        XCTAssertEqual(attrStr.string, rawString)
+        XCTAssertEqual(attrStr.getAlignmentAttributes(), [])
+        
+        // with alignment attribute
+        attrStr.addAttribute(alignment: .center)
+        XCTAssertEqual(attrStr.string, rawString)
+        XCTAssertEqual(attrStr.getAlignmentAttributes(), [.center])
+        
+    }
+    
+}
+
+fileprivate extension NSAttributedString {
+    
+    func getAlignmentAttributes() -> [NSTextAlignment] {
+        
+        var range = NSRange(location: 0, length: length)
+        
+        return attributes(at: 0, effectiveRange: &range)
+            .filter { $0.key == .paragraphStyle }
+            .values
+            .compactMap { $0 as? NSMutableParagraphStyle }
+            .map { $0.alignment }
         
     }
     
