132cfd99d42bd4220be06c6586b93a20b0a35e3d
Steffan Andrews <stef@orchetect.com>
2021-10-14 15:35:04 -0700

    Added `!` prefix operator to `NSControl.StateValue`, and `.stateValue` to `Bool`

---
+++ Sources/OTCore/Extensions/AppKit/NSControl.swift
@@ -0,0 +1,56 @@
+//
+//  NSControl.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//
+
+#if canImport(AppKit)
+
+import AppKit
+
+extension Bool {
+    
+    /// **OTCore:**
+    /// Returns `NSControl.StateValue` instance of `.on` (true) or `.off` (false).
+    public var stateValue: NSControl.StateValue {
+        
+        self ? .on : .off
+        
+    }
+    
+}
+
+extension NSControl.StateValue {
+    
+    /// **OTCore:**
+    /// Returns the inverted (toggled) state.
+    /// If `.off`, will return `.on`.
+    /// If `.on` or `.mixed`, will return `.off`.
+    public static prefix func !(stateValue: Self) -> Self {
+        
+        stateValue.toggled()
+        
+    }
+    
+    /// **OTCore:**
+    /// Returns the inverted (toggled) state.
+    /// If `.off`, will return `.on`.
+    /// If `.on` or `.mixed`, will return `.off`.
+    public func toggled() -> Self {
+        
+        self == .off ? .on : .off
+        
+    }
+    
+    /// **OTCore:**
+    /// Inverts (toggles) the state.
+    /// If `.off`, will return `.on`.
+    /// If `.on` or `.mixed`, will return `.off`.
+    public mutating func toggle() {
+        
+        self = toggled()
+        
+    }
+    
+}
+
+#endif
