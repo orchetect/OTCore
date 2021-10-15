536ad1fde523e8642deed4267e3e32f2d23ea531
Steffan Andrews <stef@orchetect.com>
2021-10-14 17:23:37 -0700

    Added pointers unit tests

---
+++ Tests/OTCoreTests/Extensions/Swift/Pointers Tests.swift
@@ -0,0 +1,32 @@
+//
+//  Pointers Tests.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//
+
+#if !os(watchOS)
+
+import XCTest
+@testable import OTCore
+
+class Extensions_Swift_Pointers_Tests: XCTestCase {
+    
+    override func setUp() { super.setUp() }
+    override func tearDown() { super.tearDown() }
+    
+    func testUnsafeRawBufferPointer_unsafeBufferPointer() {
+        
+        let data = Data([0x01, 0x02, 0x03, 0x04])
+        
+        data.withUnsafeBytes { unsafeRawBufferPtr in
+            let unsafeBufferPtr = unsafeRawBufferPtr.unsafeBufferPointer
+            XCTAssertEqual(unsafeBufferPtr[0], 0x01)
+            XCTAssertEqual(unsafeBufferPtr[1], 0x02)
+            XCTAssertEqual(unsafeBufferPtr[2], 0x03)
+            XCTAssertEqual(unsafeBufferPtr[3], 0x04)
+        }
+        
+    }
+    
+}
+
+#endif
