78a08d5f58a0f53fbe144c580fb85576f1556779
Steffan Andrews <stef@orchetect.com>
2021-12-10 01:19:55 -0800

    `NSFont`: Added `.cgFont`

---
+++ Tests/OTCoreTests/Extensions/AppKit/NSFont Tests.swift
@@ -0,0 +1,37 @@
+//
+//  NSFont Tests.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//
+
+#if os(macOS)
+
+import XCTest
+@testable import OTCore
+import AppKit
+
+class Extensions_AppKit_NSFont_Tests: XCTestCase {
+    
+    override func setUp() { super.setUp() }
+    override func tearDown() { super.tearDown() }
+    
+    func testCGFont() {
+        
+        let nsFont = NSFont.systemFont(ofSize: 10)
+        
+        let cgFont = nsFont.cgFont
+        
+        // not much we can test here, but a few properties seem testable
+        
+        XCTAssertEqual(nsFont.numberOfGlyphs, cgFont.numberOfGlyphs)
+        
+        if let nsPSN = nsFont.fontDescriptor.postscriptName,
+           let cgPSN = cgFont.postScriptName as String?
+        {
+            XCTAssert(cgPSN.hasPrefix(nsPSN))
+        }
+        
+    }
+    
+}
+
+#endif
