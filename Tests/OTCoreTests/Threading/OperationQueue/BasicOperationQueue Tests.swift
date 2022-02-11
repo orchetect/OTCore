ad7b528938313b003b10627cc2c04c4776c34e2a
Steffan Andrews <stef@orchetect.com>
2022-02-10 17:06:46 -0800

    `BasicOperationQueue`: Added `status` unit tests

--- Tests/OTCoreTests/Threading/OperationQueue/BasicOperationQueue Tests.swift
+++ Tests/OTCoreTests/Threading/OperationQueue/BasicOperationQueue Tests.swift
@@ -13,20 +13,20 @@ final class Threading_BasicOperationQueue_Tests: XCTestCase {
     /// Serial FIFO queue.
     func testOperationQueueType_serialFIFO() {
         
-        let oq = BasicOperationQueue(type: .serialFIFO)
+        let opQ = BasicOperationQueue(type: .serialFIFO)
         
-        XCTAssertEqual(oq.maxConcurrentOperationCount, 1)
+        XCTAssertEqual(opQ.maxConcurrentOperationCount, 1)
         
     }
     
     /// Automatic concurrency.
     func testOperationQueueType_automatic() {
         
-        let oq = BasicOperationQueue(type: .concurrentAutomatic)
+        let opQ = BasicOperationQueue(type: .concurrentAutomatic)
         
-        print(oq.maxConcurrentOperationCount)
+        print(opQ.maxConcurrentOperationCount)
         
-        XCTAssertEqual(oq.maxConcurrentOperationCount,
+        XCTAssertEqual(opQ.maxConcurrentOperationCount,
                        OperationQueue.defaultMaxConcurrentOperationCount)
         
     }
@@ -34,28 +34,70 @@ final class Threading_BasicOperationQueue_Tests: XCTestCase {
     /// Specific number of concurrent operations.
     func testOperationQueueType_specific() {
         
-        let oq = BasicOperationQueue(type: .concurrent(max: 2))
+        let opQ = BasicOperationQueue(type: .concurrent(max: 2))
         
-        print(oq.maxConcurrentOperationCount)
+        print(opQ.maxConcurrentOperationCount)
         
-        XCTAssertEqual(oq.maxConcurrentOperationCount, 2)
+        XCTAssertEqual(opQ.maxConcurrentOperationCount, 2)
         
     }
     
     func testLastAddedOperation() {
         
-        let oq = BasicOperationQueue(type: .serialFIFO)
-        oq.isSuspended = true
-        XCTAssertEqual(oq.lastAddedOperation, nil)
+        let opQ = BasicOperationQueue(type: .serialFIFO)
+        opQ.isSuspended = true
+        XCTAssertEqual(opQ.lastAddedOperation, nil)
         
         var op: Operation? = Operation()
-        oq.addOperation(op!)
-        XCTAssertEqual(oq.lastAddedOperation, op)
+        opQ.addOperation(op!)
+        XCTAssertEqual(opQ.lastAddedOperation, op)
+        // just FYI: op.isFinished == false here
+        // but we don't care since it doesn't affect this test
         
         op = nil
-        oq.isSuspended = false
-        oq.waitUntilAllOperationsAreFinished(timeout: .seconds(1))
-        XCTAssertEqual(oq.lastAddedOperation, nil)
+        opQ.isSuspended = false
+        wait(for: opQ.lastAddedOperation == nil, timeout: 0.2)
+        
+    }
+    
+    func testStatus() {
+        
+        let opQ = BasicOperationQueue(type: .serialFIFO)
+        
+        opQ.statusHandler = { newStatus, oldStatus in
+            print(oldStatus, newStatus)
+        }
+        
+        XCTAssertEqual(opQ.status, .idle)
+        
+        let completionBlockExp = expectation(description: "Operation Completion")
+        
+        opQ.addOperation {
+            sleep(0.1)
+            completionBlockExp.fulfill()
+        }
+        
+        switch opQ.status {
+        case .inProgress(let fractionCompleted, let message):
+            XCTAssertEqual(fractionCompleted, 0.0)
+            _ = message // don't test message content, for now
+        default:
+            XCTFail()
+        }
+        
+        wait(for: [completionBlockExp], timeout: 0.3)
+        wait(for: opQ.operationCount == 0, timeout: 0.1)
+        wait(for: opQ.progress.isFinished, timeout: 0.1)
+        
+        XCTAssertEqual(opQ.status, .idle)
+        
+        opQ.isSuspended = true
+        
+        XCTAssertEqual(opQ.status, .paused)
+        
+        opQ.isSuspended = false
+        
+        XCTAssertEqual(opQ.status, .idle)
         
     }
     
