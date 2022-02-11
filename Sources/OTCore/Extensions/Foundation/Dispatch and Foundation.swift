aabd17838da0ad4ffb3b193c1716f6ad4f201fcb
Steffan Andrews <stef@orchetect.com>
2022-02-10 16:20:35 -0800

    `DispatchTimeInterval`: Added `timeInterval` property

--- Sources/OTCore/Extensions/Foundation/Dispatch and Foundation.swift
+++ Sources/OTCore/Extensions/Foundation/Dispatch and Foundation.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  Dispatch and Foundation.swift
 //  OTCore • https://github.com/orchetect/OTCore
 //
 
@@ -7,46 +7,102 @@
 
 import Foundation // imports Dispatch
 
+// MARK: - QualityOfService / QoSClass
+
 extension QualityOfService {
+    
     /// Returns the Dispatch framework `DispatchQoS.QoSClass` equivalent.
     public var dispatchQoSClass: DispatchQoS.QoSClass {
+        
         switch self {
         case .userInteractive:
             return .userInteractive
+            
         case .userInitiated:
             return .userInitiated
+            
         case .utility:
             return .utility
+            
         case .background:
             return .background
+            
         case .default:
             return .default
+            
         @unknown default:
             return .default
         }
+        
     }
+    
 }
 
 extension DispatchQoS.QoSClass {
+    
     /// Returns the Foundation framework `QualityOfService` equivalent.
     public var qualityOfService: QualityOfService {
+        
         switch self {
         case .userInteractive:
             return .userInteractive
+            
         case .userInitiated:
             return .userInitiated
+            
         case .utility:
             return .utility
+            
         case .background:
             return .background
+            
         case .default:
             return .default
+            
         case .unspecified:
             return .default
+            
         @unknown default:
             return .default
         }
+        
     }
+    
+}
+
+// MARK: - DispatchTimeInterval
+
+extension DispatchTimeInterval {
+    
+    /// **OTCore:**
+    /// Return the interval as a `TimeInterval` (floating-point seconds).
+    public var timeInterval: TimeInterval? {
+        
+        switch self {
+        case .seconds(let val):
+            return TimeInterval(val)
+            
+        case .milliseconds(let val): // ms
+            return TimeInterval(val) / 1_000
+            
+        case .microseconds(let val): // µs
+            return TimeInterval(val) / 1_000_000
+            
+        case .nanoseconds(let val): // ns
+            return TimeInterval(val) / 1_000_000_000
+            
+        case .never:
+            //assertionFailure("Cannot convert 'never' to TimeInterval.")
+            return nil
+            
+        @unknown default:
+            assertionFailure("Unhandled DispatchTimeInterval case when attempting to convert to TimeInterval.")
+            return nil
+            
+        }
+        
+    }
+    
 }
 
 #endif
---
+++ Tests/OTCoreTests/Extensions/Foundation/Dispatch and Foundation Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Dispatch and Foundation Tests.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//
+
+#if shouldTestCurrentPlatform
+
+import XCTest
+@testable import OTCore
+
+class Extensions_Foundation_DispatchAndFoundation_Tests: XCTestCase {
+    
+    override func setUp() { super.setUp() }
+    override func tearDown() { super.tearDown() }
+    
+    // MARK: - DispatchTimeInterval
+    
+    func testDispatchTimeInterval_timeInterval() {
+        
+        
+        XCTAssertEqual(DispatchTimeInterval.seconds(2).timeInterval, 2.0)
+        
+        XCTAssertEqual(DispatchTimeInterval.milliseconds(250).timeInterval, 0.250)
+        
+        XCTAssertEqual(DispatchTimeInterval.microseconds(250).timeInterval, 0.000_250)
+        
+        XCTAssertEqual(DispatchTimeInterval.nanoseconds(250).timeInterval, 0.000_000_250)
+        
+        XCTAssertNil(DispatchTimeInterval.never.timeInterval)
+        
+        
+    }
+    
+}
+
+#endif
