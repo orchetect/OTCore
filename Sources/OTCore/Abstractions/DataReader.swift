1aad4613793315fabef7be6b657f3db99ac5694e
Steffan Andrews <stef@orchetect.com>
2022-08-17 13:09:58 -0700

    `DataReader`: internal refactor

--- Sources/OTCore/Abstractions/DataReader.swift
+++ Sources/OTCore/Abstractions/DataReader.swift
@@ -6,6 +6,7 @@
 
 import Foundation
 
+/// **OTCore:**
 /// Utility to facilitate sequential reading of bytes.
 public struct DataReader {
     public let base: Data
@@ -14,59 +15,58 @@ public struct DataReader {
         base = data
     }
     
-    /// Current byte index of read position
-    public internal(set) var readPosition = 0
+    /// Current byte index of read offset.
+    public internal(set) var readOffset = 0
     
-    /// Resets read position back to 0
+    /// Current byte index of read offset.
+    @available(
+        *,
+        deprecated,
+        message: "This property has been renamed to readOffset and will be removed in a future version of OTCore."
+    )
+    public var readPosition: Int { readOffset }
+    
+    /// Resets read offset back to 0.
     public mutating func reset() {
-        readPosition = 0
+        readOffset = 0
     }
     
-    /// Manually advance by n number of bytes from current read position
+    /// Manually advance by _n_ number of bytes from current read offset.
     public mutating func advanceBy(_ count: Int) {
-        readPosition += count
+        readOffset += count
     }
     
-    /// Return the next _n_ number of bytes and increment the read position.
+    /// Return the next _n_ number of bytes and increment the read offset.
     ///
     /// If `bytes` parameter is nil, the remainder of the data will be returned.
     ///
     /// If fewer bytes remain than are requested, `nil` will be returned.
     public mutating func read(bytes count: Int? = nil) -> Data? {
-        if count == 0 { return Data() }
-        
-        if let count = count,
-           count < 0 { return nil }
-        
-        let readPosStartIndex = base.startIndex.advanced(by: readPosition)
-        
-        let count = count ?? (base.count - readPosition)
-        
-        let endIndex = readPosStartIndex.advanced(by: count - 1)
-        
-        guard base.indices.contains(readPosStartIndex),
-              base.indices.contains(endIndex) else { return nil }
-        
-        let returnBytes = base[readPosStartIndex ... endIndex]
-        
-        // advance read position
-        readPosition += count
-        
-        return returnBytes
+        guard let d = data(bytes: count) else { return nil }
+        defer { readOffset += d.advanceCount }
+        return d.data
     }
     
-    /// Read n number of bytes from current read position, without advancing read position.
-    /// If `bytes count` passed is nil, the remainder of the data will be returned.
+    /// Read _n_ number of bytes from the current read offset, without advancing the read offset.
+    /// If `bytes count` passed is `nil`, the remainder of the data will be returned.
     /// If fewer bytes remain than are requested, `nil` will be returned.
     public func nonAdvancingRead(bytes count: Int? = nil) -> Data? {
-        if count == 0 { return Data() }
+        data(bytes: count)?.data
+    }
+    
+    // MARK: - Internal
+    
+    func data(bytes count: Int? = nil) -> (data: Data, advanceCount: Int)? {
+        if count == 0 {
+            return (data: Data(), advanceCount: 0)
+        }
         
         if let count = count,
            count < 0 { return nil }
         
-        let readPosStartIndex = base.startIndex.advanced(by: readPosition)
+        let readPosStartIndex = base.startIndex.advanced(by: readOffset)
         
-        let count = count ?? (base.count - readPosition)
+        let count = count ?? (base.count - readOffset)
         
         let endIndex = readPosStartIndex.advanced(by: count - 1)
         
@@ -75,6 +75,6 @@ public struct DataReader {
         
         let returnBytes = base[readPosStartIndex ... endIndex]
         
-        return returnBytes
+        return (data: returnBytes, advanceCount: count)
     }
 }
