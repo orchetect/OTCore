42d0c024e9f74c679a2ae10a74412f3667686849
Steffan Andrews <stef@orchetect.com>
2022-09-02 18:16:38 -0700

    Added `IPAddress` validation struct

---
+++ Sources/OTCore/Abstractions/IPAddress.swift
@@ -0,0 +1,117 @@
+//
+//  IPAddress.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+/// **OTCore:**
+/// IP Address Format Validation.
+public struct IPAddress {
+    // MARK: - Constants
+	
+    /// Internal:
+    /// IPv4 IP Address Validation Pattern.
+    private static let IPv4Pattern =
+        #"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"#
+	
+    // as suggested from: http://nbviewer.jupyter.org/github/rasbt/python_reference/blob/master/tutorials/useful_regex.ipynb#Checking-for-IP-addresses
+    /// Internal:
+    /// IPv6 IP Address Validation Pattern.
+    private static let IPv6Pattern =
+        #"^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$"#
+	
+    // MARK: - Properties
+	
+    /// **OTCore:**
+    /// IP Address.
+    public var address: String = ""
+	
+    /// **OTCore:**
+    /// Validated IP address format.
+    public var format: IPAddressType
+    
+    /// **OTCore:**
+    /// IP Address Format Validation.
+    /// If initialization succeeds, it means the IP address is of a valid format.
+    /// The ``format`` property will contain the specific IP address format.
+    public init?(_ address: String) {
+        self.address = address
+        
+        guard let validatedFormat = try? Self.validate(address: address) else {
+            return nil
+        }
+        
+        self.format = validatedFormat
+    }
+	
+    // MARK: - Methods
+    
+    /// Internal:
+    /// Performs validation on the IP address.
+    static func validate(address: String) throws -> IPAddressType {
+        // first test for IPv4
+        if try NSRegularExpression(pattern: Self.IPv4Pattern, options: [])
+            .matches(
+                in: address,
+                options: .anchored,
+                range: NSRange(
+                    location: 0,
+                    length: address.distance(
+                        from: address.startIndex,
+                        to: address.endIndex
+                    )
+                )
+            )
+            .count == 1
+        {
+            return .ipV4
+        }
+        
+        // secondly, test for IPv6
+        if try NSRegularExpression(pattern: Self.IPv6Pattern, options: [])
+            .matches(
+                in: address,
+                options: .anchored,
+                range: NSRange(
+                    location: 0,
+                    length: address.distance(
+                        from: address.startIndex,
+                        to: address.endIndex
+                    )
+                )
+            )
+            .count == 1
+        {
+            return .ipV6
+        }
+        
+        // if all tests failed, return invalid - does not match any known IP address format
+        throw ValidationError.invalid
+    }
+}
+
+extension IPAddress {
+    /// **OTCore:**
+    /// IP Address validation result returned by ``IPAddress``.
+    public enum IPAddressType {
+        /// **OTCore:**
+        /// Valid IPv4 address.
+        case ipV4
+        
+        /// **OTCore:**
+        /// Valid IPv6 address.
+        case ipV6
+    }
+    
+    /// **OTCore:**
+    /// IP Address validation error.
+    public enum ValidationError: Error {
+        case invalid
+    }
+}
+
+#endif
---
+++ Tests/OTCoreTests/Abstractions/IPAddress Tests.swift
@@ -0,0 +1,75 @@
+//
+//  IPAddress Tests.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+#if shouldTestCurrentPlatform
+
+import XCTest
+import OTCore
+
+class Abstractions_IPAddress_Tests: XCTestCase {
+    override func setUp() { super.setUp() }
+    override func tearDown() { super.tearDown() }
+    
+    func testIPAddress() {
+        // MARK: IPv4
+        
+        // valid unspecified
+        XCTAssertEqual(IPAddress("0.0.0.0")?.format, .ipV4)
+        
+        XCTAssertEqual(IPAddress("10.0.0.100")?.format, .ipV4)
+        XCTAssertEqual(IPAddress("255.255.255.255")?.format, .ipV4)
+        XCTAssertEqual(IPAddress("001.001.001.001")?.format, .ipV4)
+        
+        XCTAssertNil(IPAddress("..."))
+        XCTAssertNil(IPAddress(" . . . "))
+        XCTAssertNil(IPAddress("300.300.300.300"))
+        XCTAssertNil(IPAddress("300.1.1.1"))
+        XCTAssertNil(IPAddress("1.300.1.1"))
+        XCTAssertNil(IPAddress("1.1.300.1"))
+        XCTAssertNil(IPAddress("1.1.1.300"))
+        XCTAssertNil(IPAddress("1 .102.103.104"))
+        XCTAssertNil(IPAddress("1.2.3.4.5"))
+        
+        // no surrounding spaces allowed, even if IP address is valid
+        XCTAssertNil(IPAddress(" 1.2.3.4 "))
+        
+        // MARK: IPv6
+        
+        // local loopback address
+        XCTAssertEqual(IPAddress("::1")?.format, .ipV6)
+        
+        // valid unspecified
+        XCTAssertEqual(IPAddress("::")?.format, .ipV6)
+        
+        // includes interface component
+        XCTAssertEqual(
+            IPAddress("fe80::479:5a0d:bf0f:130%en0")?.format,
+            .ipV6
+        )
+        
+        // includes interface component
+        XCTAssertEqual(
+            IPAddress("fe80::c6a:a089:1eec:80a7%awdl0")?.format,
+            .ipV6
+        )
+        XCTAssertEqual(IPAddress("2001:470:9b36:1::2")?.format, .ipV6)
+        XCTAssertEqual(IPAddress("2001:cdba:0000:0000:0000:0000:3257:9652")?.format, .ipV6)
+        XCTAssertEqual(IPAddress("2001:cdba:0:0:0:0:3257:9652")?.format, .ipV6)
+        XCTAssertEqual(IPAddress("2001:db8:85a3::8a2e:370:7334")?.format, .ipV6)
+        
+        // IPv4 address mapped to IPv6
+        XCTAssertEqual(IPAddress("::ffff:192.0.2.128")?.format, .ipV6)
+        
+        XCTAssertNil(IPAddress("::_"))
+        
+        // uses '::' twice - not allowed
+        XCTAssertNil(IPAddress("1200::AB00:1234::2552:7777:1313"))
+        
+        XCTAssertNil(IPAddress("1200:0000:AB00:1234:O000:2552:7777:1313"))
+    }
+}
+
+#endif
