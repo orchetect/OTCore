01a5f867b0f121f7f9fa6e874ddc81d48b0042ca
Steffan Andrews <stef@orchetect.com>
2022-09-02 17:46:58 -0700

    Formatting

--- Sources/OTCore/Extensions/Darwin/Timespec.swift
+++ Sources/OTCore/Extensions/Darwin/Timespec.swift
@@ -25,11 +25,11 @@ import Darwin
 // clock that increments monotonically, in the same manner as CLOCK_MONOTONIC_RAW, but that does not increment while the system is asleep. The returned value is identical to the result of mach_absolute_time() after the appropriate mach_timebase conversion is applied.
 
 /// **OTCore:**
-/// Returns high-precision system uptime
+/// Returns high-precision system uptime.
 ///
-/// This is preferable to using `mach_absolute_time()` since `mach_absolute_time()` is macOS-only.
+/// This is preferable to using `mach_absolute_time()` since it is macOS-only.
 ///
-/// - returns: `timespec(tv_sec: Int, tv_nsec: Int)` where `tv_sec` is seconds and `tc_nsec` is nanoseconds
+/// - returns: `timespec(tv_sec: Int, tv_nsec: Int)` where `tv_sec` is seconds and `tc_nsec` is nanoseconds.
 @available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
 @inlinable @_disfavoredOverload
 public func clock_gettime_monotonic_raw() -> timespec {
@@ -46,55 +46,46 @@ public func clock_gettime_monotonic_raw() -> timespec {
 
 extension timespec {
     /// **OTCore:**
-    /// Convenience constructor from floating point seconds value
+    /// Convenience constructor from floating point seconds value.
     @inlinable @_disfavoredOverload
     public init<T: BinaryFloatingPoint>(seconds floatingPoint: T) {
         self.init()
         
         let intVal = Int(floatingPoint * 1_000_000_000)
-        
         tv_nsec = intVal % 1_000_000_000
-        
         tv_sec = intVal / 1_000_000_000
     }
 }
 
 // MARK: - Timespec operators and comparison
 
-/// **OTCore:**
-/// Add two isntances of `timespec`
-@inlinable @_disfavoredOverload
-public func + (lhs: timespec, rhs: timespec) -> timespec {
-    let nsRaw = rhs.tv_nsec + lhs.tv_nsec
-    
-    let ns = nsRaw % 1_000_000_000
-    
-    let s = lhs.tv_sec + rhs.tv_sec + (nsRaw / 1_000_000_000)
-    
-    return timespec(tv_sec: s, tv_nsec: ns)
-}
-
-/// **OTCore:**
-/// Subtract two isntances of `timespec`
-@inlinable @_disfavoredOverload
-public func - (lhs: timespec, rhs: timespec) -> timespec {
-    let nsRaw = lhs.tv_nsec - rhs.tv_nsec
-    
-    if nsRaw >= 0 {
+extension timespec {
+    /// **OTCore:**
+    /// Add two instances of `timespec`.
+    @inlinable @_disfavoredOverload
+    static public func + (lhs: timespec, rhs: timespec) -> timespec {
+        let nsRaw = rhs.tv_nsec + lhs.tv_nsec
         let ns = nsRaw % 1_000_000_000
-        
-        let s = lhs.tv_sec - rhs.tv_sec + (nsRaw / 1_000_000_000)
-        
+        let s = lhs.tv_sec + rhs.tv_sec + (nsRaw / 1_000_000_000)
         return timespec(tv_sec: s, tv_nsec: ns)
+    }
+    
+    /// **OTCore:**
+    /// Subtract two instances of `timespec`.
+    @inlinable @_disfavoredOverload
+    static public func - (lhs: timespec, rhs: timespec) -> timespec {
+        let nsRaw = lhs.tv_nsec - rhs.tv_nsec
         
-    } else {
-        // roll under
-        
-        let ns = 1_000_000_000 - (-nsRaw % 1_000_000_000)
-        
-        let s = lhs.tv_sec - rhs.tv_sec - 1 - (-nsRaw / 1_000_000_000)
-        
-        return timespec(tv_sec: s, tv_nsec: ns)
+        if nsRaw >= 0 {
+            let ns = nsRaw % 1_000_000_000
+            let s = lhs.tv_sec - rhs.tv_sec + (nsRaw / 1_000_000_000)
+            return timespec(tv_sec: s, tv_nsec: ns)
+        } else {
+            // roll under
+            let ns = 1_000_000_000 - (-nsRaw % 1_000_000_000)
+            let s = lhs.tv_sec - rhs.tv_sec - 1 - (-nsRaw / 1_000_000_000)
+            return timespec(tv_sec: s, tv_nsec: ns)
+        }
     }
 }
 
@@ -115,7 +106,6 @@ extension timespec: Comparable {
         if lhs.tv_sec > rhs.tv_sec { return false }
         
         // seconds equate; now test nanoseconds
-        
         if lhs.tv_nsec < rhs.tv_nsec { return true }
         
         return false
--- Sources/OTCore/Extensions/Swift/Collections.swift
+++ Sources/OTCore/Extensions/Swift/Collections.swift
@@ -914,7 +914,7 @@ extension Sequence {
 
 extension Collection {
     /// **OTCore:**
-    /// Splits a `Collection` or `String` into groups of `length` characters, grouping from left-to-right. If `backwards` is true, right-to-left.
+    /// Splits a `Collection` or `String` into groups of `every` _n_ number of characters, grouping from left-to-right. If `backwards` is `true`, right-to-left.
     @_disfavoredOverload
     public func split(
         every: Int,
