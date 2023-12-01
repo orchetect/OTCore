e0774d8f7f5d02661eaa494965ed17f298a074e9
Steffan Andrews <stef@orchetect.com>
2023-11-30 21:32:28 -0800

    Removed stale LinuxMain unit test file

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-//
-//  LinuxMain.swift
-//  OTCore • https://github.com/orchetect/OTCore
-//  © 2022 Steffan Andrews • Licensed under MIT License
-//
-
-fatalError("Run the tests with `swift test --enable-test-discovery`")
