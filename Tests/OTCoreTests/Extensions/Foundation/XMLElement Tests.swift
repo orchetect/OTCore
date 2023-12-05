697350f749b98db9b962b78ac53913881204d0e9
Steffan Andrews <stef@orchetect.com>
2023-12-05 13:45:21 -0800

    Added unit tests

--- Tests/OTCoreTests/Extensions/Foundation/XMLElement Tests.swift
+++ Tests/OTCoreTests/Extensions/Foundation/XMLElement Tests.swift
@@ -129,7 +129,7 @@ final class Extensions_Foundation_XMLElement_Tests: XMLTestCase {
         XCTAssertEqual(element.objectValue(forAttributeNamed: "key1") as? String, "value1")
     }
     
-    func testXMLElement_AddAttributeWithNameValue() throws {
+    func testXMLElement_AddAttributeWithNameValue() {
         let element = XMLElement(name: "test")
         
         element.addAttribute(withName: "key1", value: "value1")
@@ -138,6 +138,181 @@ final class Extensions_Foundation_XMLElement_Tests: XMLTestCase {
         // remove attribute if `nil` value is passed
         element.addAttribute(withName: "key1", value: nil)
         XCTAssertNil(element.objectValue(forAttributeNamed: "key1"))
+    }
+    
+    func testXMLElement_GetBool() {
+        let element = XMLElement(name: "testname", attributes: [
+            ("key1", "1"),
+            ("key2", "0"),
+            ("key3", "true"),
+            ("key4", "false"),
+            ("key5", "not-a-bool")
+        ])
+        
+        XCTAssertEqual(element.getBool(forAttribute: "key1"), true)
+        XCTAssertEqual(element.getBool(forAttribute: "key2"), false)
+        XCTAssertEqual(element.getBool(forAttribute: "key3"), true)
+        XCTAssertEqual(element.getBool(forAttribute: "key4"), false)
+        XCTAssertEqual(element.getBool(forAttribute: "key5"), nil)
+    }
+    
+    func testXMLElement_SetBool() {
+        let element = XMLElement(name: "testname")
+        
+        element.set(bool: true, forAttribute: "key1", useInt: false)
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "true")
+        
+        element.set(bool: false, forAttribute: "key1", useInt: false)
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "false")
+        
+        element.set(bool: true, forAttribute: "key1", useInt: true)
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "1")
+        
+        element.set(bool: false, forAttribute: "key1", useInt: true)
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "0")
+        
+        element.set(bool: nil, forAttribute: "key1", useInt: false)
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), nil)
+    }
+    
+    func testXMLElement_SetBool_removeIfDefault() {
+        let element = XMLElement(name: "testname")
+        
+        // default true
+        
+        element.set(
+            bool: true,
+            forAttribute: "key1",
+            defaultValue: true,
+            removeIfDefault: false,
+            useInt: false
+        )
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "true")
+        
+        element.set(
+            bool: true,
+            forAttribute: "key1",
+            defaultValue: true,
+            removeIfDefault: true,
+            useInt: false
+        )
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), nil)
+        
+        // default false
+        
+        element.set(
+            bool: true,
+            forAttribute: "key1",
+            defaultValue: false,
+            removeIfDefault: false,
+            useInt: false
+        )
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "true")
+        
+        element.set(
+            bool: true,
+            forAttribute: "key1",
+            defaultValue: false,
+            removeIfDefault: true,
+            useInt: false
+        )
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "true")
+        
+        // useInt: true
+        
+        element.set(
+            bool: true,
+            forAttribute: "key1",
+            defaultValue: true,
+            removeIfDefault: false,
+            useInt: true
+        )
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "1")
+        
+        element.set(
+            bool: true,
+            forAttribute: "key1",
+            defaultValue: true,
+            removeIfDefault: true,
+            useInt: true
+        )
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), nil)
+        
+        // set nil
+        
+        element.set(
+            bool: nil,
+            forAttribute: "key1",
+            defaultValue: true,
+            removeIfDefault: false,
+            useInt: false
+        )
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), nil)
+    }
+    
+    func testXMLElement_GetInt() {
+        let element = XMLElement(name: "testname", attributes: [
+            ("key1", "0"),
+            ("key2", "1"),
+            ("key3", "123"),
+            ("key4", "-10"),
+            ("key5", "123.5"),
+            ("key6", "not-an-int")
+        ])
+        
+        XCTAssertEqual(element.getInt(forAttribute: "key1"), 0)
+        XCTAssertEqual(element.getInt(forAttribute: "key2"), 1)
+        XCTAssertEqual(element.getInt(forAttribute: "key3"), 123)
+        XCTAssertEqual(element.getInt(forAttribute: "key4"), -10)
+        XCTAssertEqual(element.getInt(forAttribute: "key5"), nil)
+        XCTAssertEqual(element.getInt(forAttribute: "key6"), nil)
+    }
+    
+    func testXMLElement_SetInt() {
+        let element = XMLElement(name: "testname")
+        
+        element.set(int: 0, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "0")
+        
+        element.set(int: 1, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "1")
+        
+        element.set(int: 123, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "123")
+        
+        element.set(int: -10, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "-10")
+        
+        element.set(int: nil, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), nil)
+    }
+    
+    func testXMLElement_GetURL() {
+        let element = XMLElement(name: "testname", attributes: [
+            ("key1", "file:///Users/user/Desktop/"),
+            ("key2", "https://www.google.com"),
+            ("key3", "not-a-URL")
+        ])
+        
+        XCTAssertEqual(element.getURL(forAttribute: "key1"), URL(string: "file:///Users/user/Desktop/")!)
+        XCTAssertEqual(element.getURL(forAttribute: "key2"), URL(string: "https://www.google.com")!)
+        XCTAssertEqual(element.getURL(forAttribute: "key3"), URL(string: "not-a-URL")!) // uh... okay.
+    }
+    
+    func testXMLElement_SetURL() {
+        let element = XMLElement(name: "testname")
+        
+        element.set(url: URL(string: "file:///Users/user/Desktop/")!, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "file:///Users/user/Desktop/")
+        
+        element.set(url: URL(string: "https://www.google.com")!, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "https://www.google.com")
+        
+        element.set(url: URL(string: "not-a-URL")!, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), "not-a-URL") // uh... okay.
+        
+        element.set(url: nil, forAttribute: "key1")
+        XCTAssertEqual(element.stringValue(forAttributeNamed: "key1"), nil)
     }
     
     func testXMLElement_InitNameAttributes() {
