86a053c4cd585897e83301a90ee7da48dd13c3b9
Steffan Andrews <stef@orchetect.com>
2023-12-14 15:01:13 -0800

    Swift Concurrency: Added `withOrderedTaskGroup`

---
+++ Sources/OTCore/Extensions/Swift/Concurrency.swift
@@ -0,0 +1,36 @@
+//
+//  Concurrency.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//  © 2022 Steffan Andrews • Licensed under MIT License
+//
+
+// MARK: - Task Group
+
+/// **OTCore:**
+/// Wrapper for `withTaskGroup` that performs a task for each element of a sequence, outputting a
+/// new sequence maintaining element ordering.
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+@inlinable @_disfavoredOverload
+public func withOrderedTaskGroup<Base: Sequence, ResultElement>(
+    sequence: Base,
+    priority: TaskPriority? = nil,
+    elementTask: @escaping (_ element: Base.Element) async -> ResultElement
+) async -> [ResultElement] where /* Base.Element: Sendable, */ ResultElement: Sendable {
+    await withTaskGroup(of: (Int, ResultElement).self) { group in
+        for (number, element) in sequence.enumerated() {
+            group.addTask(priority: priority) {
+                await (number, elementTask(element))
+            }
+        }
+        
+        var dict = [Int: ResultElement](minimumCapacity: sequence.underestimatedCount)
+        
+        for await (index, item) in group {
+            dict[index] = item
+        }
+        
+        return dict
+            .sorted(by: { $0.key < $1.key })
+            .map(\.value)
+    }
+}
