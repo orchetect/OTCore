e942d263f305d262acf0a5ff3ca205db2aa8a8cd
Steffan Andrews <stef@orchetect.com>
2024-04-30 22:35:52 -0700

    `URL`: Added `sanitizeLastPathComponent(for:replacement:)` and `sanitizingLastPathComponent(for:replacement:)` methods

--- Sources/OTCore/Abstractions/String Sanitize Path Component.swift
+++ Sources/OTCore/Abstractions/String Sanitize Path Component.swift
@@ -22,12 +22,13 @@ public enum FileSystemFormat: Equatable, Hashable, CaseIterable {
 
 extension String {
     /// **OTCore:**
-    /// Sanitizes illegal characters in a path component based on the specified file system.
+    /// Sanitizes illegal characters in a path component based on the specified file system(s).
     /// This method takes an over-cautious approach by replacing certain characters that may be
     /// supported on modern systems but may still present backwards-compatibility issues.
     ///
     /// - Parameters:
-    ///   - formats: Target file system(s). Passing `nil` sanitizes the string for all supported file systems.
+    ///   - formats: Target file system(s). Passing `nil` sanitizes the string for all supported
+    ///     file systems.
     ///   - replacement: Replacement character to substitute for illegal characters.
     /// - Returns: Sanitized string.
     @_disfavoredOverload
@@ -56,7 +57,8 @@ extension String {
             // APFS:
             // Supports all valid UTF-8 strings.
             
-            // Even though HFS+ and APFS file systems both support all Unicode characters, most APIs provided
+            // Even though HFS+ and APFS file systems both support all Unicode characters, most APIs
+            // provided
             // by OS X/macOS allow you create/open files with a slash `/` or NUL `\0` in them.
             
             let illegalChars: [Character] = ["/", "\0", ":"]
@@ -67,13 +69,13 @@ extension String {
     
     /// **OTCore:**
     /// Returns the path component string by sanitizing illegal characters based on the specified
-    /// file system. This method takes an over-cautious approach by replacing certain characters
+    /// file system(s). This method takes an over-cautious approach by replacing certain characters
     /// that may be supported on modern systems but may still present backwards-compatibility
     /// issues.
     ///
     /// - Parameters:
     ///   - formats: Target file system(s). Passing `nil` sanitizes the string for all supported
-    /// file systems.
+    ///     file systems.
     ///   - replacement: Replacement character to substitute for illegal characters.
     /// - Returns: Sanitized string.
     @_disfavoredOverload
@@ -86,3 +88,54 @@ extension String {
         return mutable
     }
 }
+
+#if canImport(Foundation)
+
+import Foundation
+
+extension URL {
+    /// **OTCore:**
+    /// Returns the URL by sanitizing illegal characters in the last path component based on the
+    /// specified file system(s). This method takes an over-cautious approach by replacing certain
+    /// characters that may be supported on modern systems but may still present
+    /// backwards-compatibility issues.
+    ///
+    /// - Parameters:
+    ///   - formats: Target file system(s). Passing `nil` sanitizes the string for all supported
+    ///     file systems.
+    ///   - replacement: Replacement character to substitute for illegal characters.
+    @_disfavoredOverload
+    public mutating func sanitizeLastPathComponent(
+        for formats: [FileSystemFormat]? = nil,
+        replacement: Character = "-"
+    ) {
+        let lpc = lastPathComponent
+            .sanitizingPathComponent(for: formats, replacement: replacement)
+        
+        deleteLastPathComponent()
+        
+        appendPathComponent(lpc)
+    }
+    
+    /// **OTCore:**
+    /// Sanitizes illegal characters in the last path component based on the
+    /// specified file system(s). This method takes an over-cautious approach by replacing certain
+    /// characters that may be supported on modern systems but may still present
+    /// backwards-compatibility issues.
+    ///
+    /// - Parameters:
+    ///   - formats: Target file system(s). Passing `nil` sanitizes the string for all supported
+    ///     file systems.
+    ///   - replacement: Replacement character to substitute for illegal characters.
+    @_disfavoredOverload
+    public func sanitizingLastPathComponent(
+        for formats: [FileSystemFormat]? = nil,
+        replacement: Character = "-"
+    ) -> URL {
+        var mutable = self
+        mutable.sanitizeLastPathComponent(for: formats, replacement: replacement)
+        return mutable
+    }
+}
+
+#endif
