fd2809b36af73f0a729eda41dec378ac78b1b0a3
Steffan Andrews <stef@orchetect.com>
2024-04-30 22:36:06 -0700

    Updated unit tests

--- Tests/OTCoreTests/Abstractions/String Sanitize Path Component Tests.swift
+++ Tests/OTCoreTests/Abstractions/String Sanitize Path Component Tests.swift
@@ -9,6 +9,7 @@
 import XCTest
 import OTCore
 
+@available(macOS 13, iOS 16, tvOS 16, watchOS 9, *)
 class Abstractions_StringSanitizePathComponent_Tests: XCTestCase {
     override func setUp() { super.setUp() }
     override func tearDown() { super.tearDown() }
@@ -28,12 +29,42 @@ class Abstractions_StringSanitizePathComponent_Tests: XCTestCase {
     private func check(fileSystem: FileSystemFormat?) {
         let fs: [FileSystemFormat]? = fileSystem != nil ? [fileSystem!] : nil
         
+        // path component string
         XCTAssertEqual("".sanitizingPathComponent(for: fs, replacement: "-"), "")
         XCTAssertEqual("Test/File.txt".sanitizingPathComponent(for: fs, replacement: "-"), "Test-File.txt")
         XCTAssertEqual("Test//File.txt".sanitizingPathComponent(for: fs, replacement: "-"), "Test--File.txt")
         XCTAssertEqual("TestFile\0.txt".sanitizingPathComponent(for: fs, replacement: "-"), "TestFile-.txt")
         XCTAssertEqual("Test/File\0.txt".sanitizingPathComponent(for: fs, replacement: "-"), "Test-File-.txt")
         XCTAssertEqual("Test:File.txt".sanitizingPathComponent(for: fs, replacement: "-"), "Test-File.txt")
+        
+        // url last path component
+        XCTAssertEqual(
+            URL(fileURLWithPath: "/Folder").appending(component: "").sanitizingLastPathComponent(for: fs, replacement: "-").path,
+            "/Folder"
+        )
+        XCTAssertEqual(
+            URL(fileURLWithPath: "/Folder").appending(component: "Test/File.txt").sanitizingLastPathComponent(for: fs, replacement: "-").path,
+            "/Folder/Test/File.txt" // URL interprets `/` as a path delimiter
+        )
+        XCTAssertEqual(
+            URL(fileURLWithPath: "/Folder").appending(component: "Test//File.txt").sanitizingLastPathComponent(for: fs, replacement: "-").path,
+            "/Folder/Test//File.txt" // URL interprets `/` as a path delimiter
+        )
+        
+        // Note: appending a null character to URL will cause a crash
+        // XCTAssertEqual(
+        //     URL(fileURLWithPath: "/Folder").appending(component: "TestFile\0.txt").sanitizingLastPathComponent(for: fs, replacement: "-").path,
+        //     "/Folder/TestFile-.txt"
+        // )
+        // XCTAssertEqual(
+        //     URL(fileURLWithPath: "/Folder").appending(component: "Test/File\0.txt").sanitizingLastPathComponent(for: fs, replacement: "-").path,
+        //     "/Folder/Test-File-.txt"
+        // )
+        
+        XCTAssertEqual(
+            URL(fileURLWithPath: "/Folder").appending(component: "Test:File.txt").sanitizingLastPathComponent(for: fs, replacement: "-").path,
+            "/Folder/Test-File.txt"
+        )
     }
 }
 
