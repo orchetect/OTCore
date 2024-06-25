6af4d4fb3204a0fb87cfb449dd46dc4fd2b92c46
Steffan Andrews <stef@orchetect.com>
2024-06-24 21:39:05 -0700

    Added 1.5.4 API evolution

---
+++ Sources/OTCore/API Evolution/OTCore API 1.5.4.swift
@@ -0,0 +1,33 @@
+//
+//  OTCore API 1.5.4.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//  © 2024 Steffan Andrews • Licensed under MIT License
+//
+
+// MARK: - URL.swift
+
+#if canImport(Foundation)
+import Foundation
+
+extension FileManager {
+    #if os(macOS)
+    
+    /// **OTCore:**
+    /// Backwards compatible method for retrieving the current user's home directory, using the most recent API where possible.
+    @available(*, deprecated, renamed: "default.homeDirectoryForCurrentUserCompat")
+    @_disfavoredOverload
+    public static var homeDirectoryForCurrentUserCompat: URL {
+        Self.default.homeDirectoryForCurrentUserCompat
+    }
+    
+    #endif
+    
+    /// **OTCore:**
+    /// Backwards compatible method for retrieving a temporary folder from the system.
+    @available(*, deprecated, renamed: "default.temporaryDirectoryCompat")
+    @_disfavoredOverload
+    public static var temporaryDirectoryCompat: URL {
+        Self.default.temporaryDirectoryCompat
+    }
+}
+#endif
