4d3452de7b716201b3f81530e5a793dd84532cf8
Steffan Andrews <stef@orchetect.com>
2024-10-30 01:16:56 -0700

    `FormatStyle`: Added `plainInteger`

---
+++ Sources/OTCore/Extensions/Foundation/FormatStyle.swift
@@ -0,0 +1,27 @@
+//
+//  FormatStyle.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//  © 2024 Steffan Andrews • Licensed under MIT License
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+@available(macOS 12.0, iOS 15.0, tvOS 15.0, watchOS 8.0, *)
+extension FormatStyle where Self == IntegerFormatStyle<Int> {
+    /// Plain integer format style.
+    ///
+    /// Integer with no decimal places, without grouping (thousands separators), and without sign.
+    ///
+    /// Suitable for technical integer input, including network port numbers.
+    @_disfavoredOverload
+    public static var plainInteger: IntegerFormatStyle<Int> {
+        IntegerFormatStyle<Int>()
+            .precision(.fractionLength(0))
+            .grouping(.never)
+            .sign(strategy: .never)
+    }
+}
+
+#endif
