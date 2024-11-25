0d839aacf9dc25ec5a255eb6b9b5fa3c072ca96f
Steffan Andrews <stef@orchetect.com>
2024-11-24 23:33:27 -0800

    Package.swift cleanup

--- Package.swift
+++ Package.swift
@@ -4,14 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "OTCore",
-    
-    // certain features of the library are marked @available only on newer versions of OSes,
-    // but a platforms spec here determines what base platforms
-    // the library is currently supported on
     platforms: [
+        // certain features of the library are marked @available only on newer versions of OSes,
+        // but a platforms spec here determines what base platforms
+        // the library is currently supported on
         .macOS(.v10_12), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
     ],
-    
     products: [
         .library(
             name: "OTCore",
@@ -19,19 +17,16 @@ let package = Package(
             targets: ["OTCore"]
         )
     ],
-    
     dependencies: [
         // testing-only dependency
         .package(url: "https://github.com/orchetect/XCTestUtils", from: "1.0.3")
     ],
-    
     targets: [
         .target(
             name: "OTCore",
             dependencies: [],
             swiftSettings: [.define("DEBUG", .when(configuration: .debug))]
         ),
-        
         .testTarget(
             name: "OTCoreTests",
             dependencies: ["OTCore", "XCTestUtils"]
