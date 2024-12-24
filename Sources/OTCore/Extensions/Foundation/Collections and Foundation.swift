bc5deb5b61e3d27811bf525301830d332a7cd0dd
Steffan Andrews <stef@orchetect.com>
2024-12-24 15:56:10 -0800

    `ComparisonResult`: Added `inverted` property

---
+++ Sources/OTCore/Extensions/Foundation/Collections and Foundation.swift
@@ -0,0 +1,26 @@
+//
+//  Collections and Foundation.swift
+//  OTCore • https://github.com/orchetect/OTCore
+//  © 2024 Steffan Andrews • Licensed under MIT License
+//
+
+#if canImport(Foundation)
+
+import Foundation
+
+// MARK: - Comparison
+
+extension ComparisonResult {
+    /// **OTCore:**
+    /// Returns the inverted comparison result.
+    @inlinable @_disfavoredOverload
+    public var inverted: Self {
+        switch self {
+        case .orderedAscending: .orderedDescending
+        case .orderedSame: .orderedSame
+        case .orderedDescending: .orderedAscending
+        }
+    }
+}
+
+#endif
