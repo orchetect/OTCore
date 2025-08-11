d9071109140f825477ce51a1ed9335670f85a93b
Steffan Andrews <stef@orchetect.com>
2025-08-11 15:41:21 -0700

    `timespec`: `init(_: TimeInterval)`: Updated inline docs

--- Sources/OTCore/Extensions/Foundation/Timespec and TimeInterval.swift
+++ Sources/OTCore/Extensions/Foundation/Timespec and TimeInterval.swift
@@ -12,7 +12,20 @@ import Foundation
 
 extension timespec {
     /// **OTCore:**
-    /// Convenience constructor from `TimeInterval`
+    /// Convenience constructor from `TimeInterval`.
+    ///
+    /// > Note:
+    /// > As of macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, Swift adds a new `Duration` type allowing this
+    /// > functionality to be used natively.
+    /// >
+    /// > For example:
+    /// >
+    /// > ```swift
+    /// > timespec(.seconds(1.5))
+    /// > timespec(.milliseconds(1500))
+    /// > timespec(.microseconds(1_500_000))
+    /// > timespec(.nanoseconds(1_500_000_000))
+    /// > ```
     @inlinable @_disfavoredOverload
     public init(_ interval: TimeInterval) {
         self.init(seconds: interval)
