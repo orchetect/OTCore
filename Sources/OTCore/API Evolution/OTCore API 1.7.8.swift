35de61e000351b8106c70998109ec7fd751042a0
Steffan Andrews <stef@orchetect.com>
2025-08-11 14:28:40 -0700

    `NSPoint`: Removed superfluous `cgPoint` property

--- Sources/OTCore/API Evolution/OTCore API 1.7.8.swift
+++ Sources/OTCore/API Evolution/OTCore API 1.7.8.swift
@@ -1,7 +1,7 @@
 //
-//  NSPoint.swift
+//  OTCore API 1.7.8.swift
 //  OTCore • https://github.com/orchetect/OTCore
-//  © 2022 Steffan Andrews • Licensed under MIT License
+//  © 2024 Steffan Andrews • Licensed under MIT License
 //
 
 #if canImport(AppKit)
@@ -11,6 +11,7 @@ import Foundation // imports Core Graphics
 extension NSPoint {
     /// **OTCore:**
     /// Returns the `NSPoint` as a `CGPoint` (toll-free bridged).
+    @available(*, deprecated, message: "This property will be removed in future. NSPoint is toll-free bridged to CGPoint, use it as-is.")
     @_disfavoredOverload
     public var cgPoint: CGPoint {
         self as CGPoint
