2d4ceda5dc7b098b7949738340f99deb8d7b2d5d
Steffan Andrews <stef@orchetect.com>
2025-08-11 14:28:50 -0700

    Updated unit tests

--- Tests/OTCoreTests/Extensions/AppKit/NSPoint Tests.swift
+++ Tests/OTCoreTests/Extensions/AppKit/NSPoint Tests.swift
@@ -14,8 +14,10 @@ class Extensions_Foundation_NSPoint_Tests: XCTestCase {
     override func setUp() { super.setUp() }
     override func tearDown() { super.tearDown() }
     
+    /// This method used to test an OTCore category property `cgPoint` which was removed in OTCore 1.7.8
     func testCGPoint() {
-        let cgPoint = NSPoint(x: 1.23, y: 2.5).cgPoint
+        // just to confirm that the compiler sees both types as the same
+        let cgPoint: CGPoint = NSPoint(x: 1.23, y: 2.5)
         
         XCTAssertEqual(cgPoint.x, 1.23)
         XCTAssertEqual(cgPoint.y, 2.5)
