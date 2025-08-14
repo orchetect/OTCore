80d3767ad5b0e8f9da0f6cd2fa6fb0489823e062
Steffan Andrews <stef@orchetect.com>
2025-08-13 23:34:10 -0700

    Added `withOrderedThrowingTaskGroup` global method

--- Sources/OTCore/Extensions/Swift/Concurrency.swift
+++ Sources/OTCore/Extensions/Swift/Concurrency.swift
@@ -7,8 +7,7 @@
 // MARK: - Task Group
 
 /// **OTCore:**
-/// Wrapper for `withTaskGroup` that performs a task for each element of a sequence, outputting a
-/// new sequence maintaining element ordering.
+/// Wrapper for `withTaskGroup` that performs a task for each element of a sequence, outputting a new sequence maintaining element ordering.
 @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
 @inlinable @_disfavoredOverload
 public func withOrderedTaskGroup<Base: Sequence, ResultElement>(
@@ -27,6 +26,35 @@ public func withOrderedTaskGroup<Base: Sequence, ResultElement>(
         var dict = [Int: ResultElement](minimumCapacity: sequence.underestimatedCount)
         
         for await (index, item) in group {
+            dict[index] = item
+        }
+        
+        return dict
+            .sorted(by: { $0.key < $1.key })
+            .map(\.value)
+    }
+}
+
+/// **OTCore:**
+/// Wrapper for `withThrowingTaskGroup` that performs a task for each element of a sequence, outputting a new sequence maintaining element ordering.
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+@inlinable @_disfavoredOverload
+public func withOrderedThrowingTaskGroup<Base: Sequence, ResultElement>(
+    sequence: Base,
+    priority: TaskPriority? = nil,
+    isolation: isolated (any Actor)? = #isolation,
+    elementTask: @Sendable @escaping @isolated(any) (_ element: Base.Element) async throws -> ResultElement
+) async rethrows -> [ResultElement] where Base.Element: Sendable, ResultElement: Sendable {
+    try await withThrowingTaskGroup(of: (Int, ResultElement).self, isolation: isolation) { group in
+        for (number, element) in sequence.enumerated() {
+            group.addTask(priority: priority) {
+                try await (number, elementTask(element))
+            }
+        }
+        
+        var dict = [Int: ResultElement](minimumCapacity: sequence.underestimatedCount)
+        
+        for try await (index, item) in group {
             dict[index] = item
         }
         
